5e8e2685d3ca7ae7d0a34e082f8e1ea57918ea8a
JerryALT <kirill1kirill2kirill3@gmail.com>
2023-10-10 22:58:55 +0300

    [Events]: Add 'OnVMShutdown' event

--- src/Components/Modules/Events.cpp
+++ src/Components/Modules/Events.cpp
@@ -3,10 +3,16 @@
 namespace Components
 {
 	Utils::Signal<Events::Callback> Events::DvarInitSignal;
+	Utils::Signal<Events::Callback> Events::ShutdownSystemTasks;
 
 	void Events::OnDvarInit(const Utils::Slot<Callback>& callback)
 	{
 		DvarInitSignal.connect(callback);
+	}
+
+	void Events::OnVMShutdown(const std::function<void()>& callback)
+	{
+		ShutdownSystemTasks.connect(callback);
 	}
 
 	void Events::Com_InitDvars_stub()
@@ -17,9 +23,20 @@ namespace Components
 		Utils::Hook::Call<void()>(0x534630)(); // Com_InitDvars
 	}
 
+	void Events::Scr_ShutdownSystem_Hk(unsigned char sys)
+	{
+		ShutdownSystemTasks();
+		ShutdownSystemTasks.clear();
+
+		Utils::Hook::Call<void(unsigned char)>(0x557D00)(sys); // Scr_ShutdownSystem
+	}
+
 	Events::Events()
 	{
 		Utils::Hook(0x534B15, Com_InitDvars_stub, HOOK_CALL).install()->quick();
+
+		Utils::Hook(0x4C5478, Scr_ShutdownSystem_Hk, HOOK_CALL).install()->quick(); // G_LoadGame
+		Utils::Hook(0x4B9C13, Scr_ShutdownSystem_Hk, HOOK_CALL).install()->quick(); // G_ShutdownGame
 	}
 
 	Events::~Events()
--- src/Components/Modules/Events.hpp
+++ src/Components/Modules/Events.hpp
@@ -11,8 +11,12 @@ namespace Components
 		~Events();
 
 		static void OnDvarInit(const Utils::Slot<Callback>& callback);
+		static void OnVMShutdown(const Utils::Slot<Callback>& callback);
 	private:
 		static Utils::Signal<Callback> DvarInitSignal;
+		static Utils::Signal<Callback> ShutdownSystemTasks;
+
 		static void Com_InitDvars_stub();
+		static void Scr_ShutdownSystem_Hk(unsigned char sys);
 	};
 }
